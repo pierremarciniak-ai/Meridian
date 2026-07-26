bd2a05be7111a6660872e32ba5999b95b58d6b4d
Pierre Marciniak <pierremarciniak@gmail.com>
2026-07-26 17:37:34 +0200

    install hardhat

---
+++ backend/contracts/Meridian.sol
@@ -0,0 +1,12 @@
+// SPDX-License-Identifier: MIT
+
+pragma solidity 0.8.28;
+import "@openzeppelin/contracts/access/Ownable.sol";
+import "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+
+
+contract Meridian is Ownable {
+
+    
+
+}
